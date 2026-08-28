0c4925a9db419fadc37a6fe04db68b5773cb337c
Tadiwa Marufu <tadiwamarufu00@gmail.com>
2026-08-28 16:34:26 +0200

    Add crash logger for debugging

--- app/src/main/java/com/thepurpleweb/purplelauncher/MainActivity.kt
+++ app/src/main/java/com/thepurpleweb/purplelauncher/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.thepurpleweb.purplelauncher
 
 import android.os.Bundle
+import android.os.Environment
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.thepurpleweb.purplelauncher.profile.ProfileEngine
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+            try {
+                val sw = StringWriter()
+                throwable.printStackTrace(PrintWriter(sw))
+                val file = File(getExternalFilesDir(null), "crash_log.txt")
+                file.writeText(sw.toString())
+            } catch (e: Exception) {
+                // ignore, nothing more we can do
+            }
+            android.os.Process.killProcess(android.os.Process.myPid())
+        }
+
         super.onCreate(savedInstanceState)
 
         profileEngine = ProfileEngine(applicationContext)
